535eb7e7a667dfb9c344b6baae97ece6a817ef90
ayobamiseun <adegokeayobamiseun@gmail.com>
2023-01-11 11:22:51 +0100

    routes and pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers'
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-      <h1>hello</h1>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+
+        </Routes>
+      </Router>
+    </>
+
   );
 }
 
