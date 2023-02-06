b95b5cf90203d1b5fc6666e22da6004d5333666c
ayobamiseun <adegokeayobamiseun@gmail.com>
2023-02-06 15:54:09 +0100

    delete functionality

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Moment from 'react-moment'
 import { Link } from 'react-router-dom'
-import { MdLocationOn } from 'react-icons/md'
+import { MdLocationOn, MdEdit } from 'react-icons/md'
+import {FaTrash} from 'react-icons/fa'
 
-export default function ListingItem({listing, id}) {
+
+export default function ListingItem({listing, id, onEdit, onDelete}) {
   return (<>
      <li className='relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]'>
          <Link className='content'  to={`/categories/${listing.type}/${id}`} >
@@ -40,6 +42,11 @@ export default function ListingItem({listing, id}) {
          </div>
          
          </Link>
+         {onDelete &&( <FaTrash className='absolute bottom-2 right-2 h-[14px] cursor-pointer text-red-500' onClick={()=> onDelete(listing.id)} />) }
+         {onEdit && (<MdEdit className='absolute bottom-2 right-8 h-[14px] cursor-pointer '  onClick={()=> onEdit(listing.id)} />)}
+         
+         
+         
       </li>
     
   </>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { getAuth, updateProfile } from "firebase/auth";
 import {
   collection,
+  deleteDoc,
   doc,
   getDoc,
   getDocs,
@@ -81,6 +82,21 @@ export default function Profile() {
     fetchUserListing();
   }, [auth.currentUser.uid]);
 
+  async function onDelete(listingID){
+    if(window.confirm("Are you sure you want to Delete")) {
+       await deleteDoc(doc(db, "listings",  listingID))
+       const updatedListings = listings.filter(
+        (listing) => listing.id !==  listingID
+       );
+       setListings(updatedListings);
+
+       toast.success("successfully Deleted")
+    }
+  }
+  function onEdit(listingID){
+     navigate(`/edit-listing/${listingID}`)
+  }
+
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -152,6 +168,9 @@ export default function Profile() {
                   key={listing.id}
                   id={listing.id}
                   listing={listing.data}
+                  onDelete={()=> onDelete(listing.id)}
+                  onEdit={()=> onEdit(listing.id)}
+                  
                 />
               ))}
             </ul>
