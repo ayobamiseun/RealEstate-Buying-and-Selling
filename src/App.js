23a4fa184512ac3ea3bb358bc6d39a8254c06725
ayobamiseun <adegokeayobamiseun@gmail.com>
2023-02-07 08:15:20 +0100

    Edit Posts

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Header from './components/Header';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import CreateListing from './pages/CreateListing';
+import EditListing from './pages/EditListing';
 
 
 function App() {
@@ -33,6 +34,10 @@ function App() {
 
           <Route path='/create-listing' element={<PrivateRoute />} >
           <Route path='/create-listing' element={<CreateListing />} />
+          </Route>
+
+          <Route path='/edit-listing' element={<PrivateRoute />} >
+          <Route path='/edit-listing/:listingID' element={<EditListing />} />
           </Route>
           
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -82,19 +82,19 @@ export default function Profile() {
     fetchUserListing();
   }, [auth.currentUser.uid]);
 
-  async function onDelete(listingID){
-    if(window.confirm("Are you sure you want to Delete")) {
-       await deleteDoc(doc(db, "listings",  listingID))
-       const updatedListings = listings.filter(
-        (listing) => listing.id !==  listingID
-       );
-       setListings(updatedListings);
+  async function onDelete(listingID) {
+    if (window.confirm("Are you sure you want to Delete")) {
+      await deleteDoc(doc(db, "listings", listingID));
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingID
+      );
+      setListings(updatedListings);
 
-       toast.success("successfully Deleted")
+      toast.success("successfully Deleted");
     }
   }
-  function onEdit(listingID){
-     navigate(`/edit-listing/${listingID}`)
+  function onEdit(listingID) {
+    navigate(`/edit-listing/${listingID}`);
   }
 
   return (
@@ -161,16 +161,17 @@ export default function Profile() {
       <div className="max-w-6xl px-3 mt-6 mx-auto">
         {!loading && listings.length > 0 && (
           <>
-            <h2 className="text-2xl text-center font-semibold mb-6">My Listing</h2>
+            <h2 className="text-2xl text-center font-semibold mb-6">
+              My Listing
+            </h2>
             <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6">
               {listings.map((listing) => (
                 <ListingItem
                   key={listing.id}
                   id={listing.id}
                   listing={listing.data}
-                  onDelete={()=> onDelete(listing.id)}
-                  onEdit={()=> onEdit(listing.id)}
-                  
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
